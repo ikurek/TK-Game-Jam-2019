a269a08691ee6232bbeafd5c5ef742b026891e4b
Igor Kurek <igorkurek96@gmail.com>
2019-02-24 00:44:05 +0100

    Added handler to detect picked object manually

--- Assets/BoardObject/BonfireScript.cs
+++ Assets/BoardObject/BonfireScript.cs
@@ -9,21 +9,29 @@ public class BonfireScript : InteractiveObject
 
     public bool pickable = false;
     
-    public override void pickup(GameObject parentGameObject)
+    public override GameObject pickup(GameObject parentGameObject)
     {
         if (pickable)
         {
             base.playSound("Sound/pick_wood");
-            base.pickup(parentGameObject);
+            return base.pickup(parentGameObject);
+        }
+        else
+        {
+            return null;
         }
     }
 
-    public override void drop(GameObject exParentGameObject)
+    public override GameObject drop(GameObject exParentGameObject)
     {
         if (pickable)
         {
             base.playSound("Sound/drop_wood");
-            base.drop(exParentGameObject);
+            return base.drop(exParentGameObject);
+        }
+        else
+        {
+            return null;
         }
     }
 
