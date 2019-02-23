a14282ee8e0ffa174e5e287724ad987245a2c620
outfrost <kotlet.bahn@gmail.com>
2019-02-23 10:42:12 +0100

    We have a player character and it works and it's awesome ✨

---
+++ Assets/Player/PlayerCharacter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using World;
+
+public class PlayerCharacter : MonoBehaviour {
+	
+	[Flags]
+	private enum MovementDirection {
+		None = 0,
+		North = 1 << 0,
+		East = 1 << 1,
+		South = 1 << 2,
+		West = 1 << 3
+	}
+	
+	public float movementSpeed;
+	
+	private new Collider2D collider;
+	private MovementDirection movementDirection = MovementDirection.None;
+	
+	private void Awake() {
+		collider = GetComponent<Collider2D>();
+		if (collider == null) {
+			Debug.LogError("No Collider2D found in Player Character");
+		}
+	}
+	
+	private void Start() {
+		InputDispatcher dispatcher = FindObjectOfType<InputDispatcher>(); 
+		dispatcher.AddKeyDownHandler(KeyCode.W, keyCode => startMovement(MovementDirection.North));
+		dispatcher.AddKeyDownHandler(KeyCode.D, keyCode => startMovement(MovementDirection.East));
+		dispatcher.AddKeyDownHandler(KeyCode.S, keyCode => startMovement(MovementDirection.South));
+		dispatcher.AddKeyDownHandler(KeyCode.A, keyCode => startMovement(MovementDirection.West));
+		dispatcher.AddKeyUpHandler(KeyCode.W, keyCode => stopMovement(MovementDirection.North));
+		dispatcher.AddKeyUpHandler(KeyCode.D, keyCode => stopMovement(MovementDirection.East));
+		dispatcher.AddKeyUpHandler(KeyCode.S, keyCode => stopMovement(MovementDirection.South));
+		dispatcher.AddKeyUpHandler(KeyCode.A, keyCode => stopMovement(MovementDirection.West));
+	}
+	
+	private void Update() {
+		Vector2 velocity = directionVector(movementDirection) * movementSpeed;
+		Vector2 move = velocity * Time.deltaTime;
+		
+		/******************************************
+		 * This here is magic that fixes Unity's bullshit collision detection
+		 */
+		RaycastHit2D[] hits = new RaycastHit2D[64];
+		int numHits = collider.Cast(move.normalized, hits, move.magnitude);
+		
+		for (int i = 0; i < numHits; i++) {
+			float collisionNormalDot = Vector2.Dot(move.normalized, hits[i].normal);
+			move -= hits[i].normal * collisionNormalDot * move.magnitude; 
+			
+		}
+		/******************************************
+		 */
+		
+		transform.position += (Vector3) move;
+	}
+	
+	private void startMovement(MovementDirection direction) {
+		movementDirection |= direction;
+	}
+	
+	private void stopMovement(MovementDirection direction) {
+		movementDirection &= ~direction;
+	}
+	
+	private Vector2 directionVector(MovementDirection direction) {
+		Vector2 vec = Vector2.zero;
+		if (direction != MovementDirection.None) {
+			if (direction.HasFlag(MovementDirection.North)) vec += Vector2.up;
+			if (direction.HasFlag(MovementDirection.East)) vec += Vector2.right;
+			if (direction.HasFlag(MovementDirection.South)) vec += Vector2.down;
+			if (direction.HasFlag(MovementDirection.West)) vec += Vector2.left;
+			vec = vec.normalized;
+		}
+		return vec;
+	}
+	
+}
--- Assets/World/InputDispatcher.cs
+++ Assets/World/InputDispatcher.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace World {
-
+	
 	public class InputDispatcher : MonoBehaviour {
 		
 		private readonly Dictionary<KeyCode, LinkedList<Action<KeyCode>>> keyDownHandlers = new Dictionary<KeyCode, LinkedList<Action<KeyCode>>>();
+		private readonly Dictionary<KeyCode, LinkedList<Action<KeyCode>>> keyUpHandlers = new Dictionary<KeyCode, LinkedList<Action<KeyCode>>>();
 		
 		private void Update() {
 			// Scan registered key inputs and invoke handlers
+			
 			foreach (var entry in keyDownHandlers) {
 				if (Input.GetKeyDown(entry.Key)) {
+					foreach (var handler in entry.Value) {
+						handler(entry.Key);
+					}
+				}
+			}
+			
+			foreach (var entry in keyUpHandlers) {
+				if (Input.GetKeyUp(entry.Key)) {
 					foreach (var handler in entry.Value) {
 						handler(entry.Key);
 					}
@@ -24,6 +34,19 @@ namespace World {
 			try {
 				list = new LinkedList<Action<KeyCode>>();
 				keyDownHandlers.Add(keyCode, list);
+			}
+			catch (ArgumentException) {
+				list = keyDownHandlers[keyCode];
+			}
+			
+			list.AddLast(handler);
+		}
+		
+		public void AddKeyUpHandler(KeyCode keyCode, Action<KeyCode> handler) {
+			LinkedList<Action<KeyCode>> list;
+			try {
+				list = new LinkedList<Action<KeyCode>>();
+				keyUpHandlers.Add(keyCode, list);
 			}
 			catch (ArgumentException) {
 				list = keyDownHandlers[keyCode];
