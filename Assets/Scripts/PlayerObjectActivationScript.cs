1e2d07c51a28a407790164b5d4cf7e015085a866
Igor Kurek <igorkurek96@gmail.com>
2019-02-23 03:07:54 +0100

    Added script to activate object

---
+++ Assets/Scripts/PlayerObjectActivationScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using World;
+
+public class PlayerObjectActivationScript : MonoBehaviour
+{
+    string interactiveTag = "interactive";
+    bool isTouchingInteractive = false;
+    GameObject lastCollidedInteractive = null;
+
+    void Start()
+    {
+        FindObjectOfType<InputDispatcher>().AddKeyDownHandler(KeyCode.Space, (KeyCode) => {
+            activateLastCollidedInteractive();
+        });
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        if(collision.gameObject.tag == interactiveTag) {
+            isTouchingInteractive = true;
+            lastCollidedInteractive = collision.gameObject;
+        }
+    }
+ 
+    void OnCollisionExit(Collision collision) 
+    {
+        if(collision.gameObject.tag == interactiveTag) {
+            isTouchingInteractive = false;
+        }
+    }
+
+    void activateLastCollidedInteractive()
+    {
+        if(isTouchingInteractive) {
+            IInteractiveObject interactive = lastCollidedInteractive.GetComponent<InteractiveObjectScript>() as IInteractiveObject;
+            interactive.activate();
+        }
+
+    }
+}
