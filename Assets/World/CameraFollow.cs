dd3e2ae368f2320279ed56cab67be7246f25c480
Igor Kurek <igorkurek96@gmail.com>
2019-02-23 21:17:12 +0100

    Added script to follow player with camera

---
+++ Assets/World/CameraFollow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+ 
+public class CameraFollow : MonoBehaviour {
+     
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    private Camera camera;
+    public Transform target;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if (target)
+        {
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+     
+    }
+}
