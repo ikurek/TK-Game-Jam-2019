d16735a6a544f5743fbf0b9b192298e56505a872
Wojciech Wojcik <wojtek.wroclaw@hotmail.com>
2019-02-23 22:01:39 +0100

    working on lvl 3, switches working

---
+++ Assets/Puzzles/Modern/BulbSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbSwitcher : InteractiveObject
+{
+    [SerializeField]
+    private int switchNumber;
+
+    [SerializeField]
+    private GameObject parent;
+    
+    public override void activate(GameObject activator)
+    {
+        switch (switchNumber)
+        {
+            case 1:
+                parent.GetComponent<LightBulbsPuzzle>().firstSwitch();
+                break;
+            case 2:
+                parent.GetComponent<LightBulbsPuzzle>().secondSwitch();
+                break;
+            case 3:
+                parent.GetComponent<LightBulbsPuzzle>().thirdSwitch();
+                break;
+        }
+        
+        base.activate(activator);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
