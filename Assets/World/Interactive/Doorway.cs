31b0ac2f0aedf5c37636864e80dd1c1176281a66
outfrost <kotlet.bahn@gmail.com>
2019-02-24 02:11:37 +0100

    Implement door teleportation

---
+++ Assets/World/Interactive/Doorway.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorway : InteractiveObject {
+
+	public Vector2 teleportExitPosition;
+	public Doorway otherEnd;
+	public string characterTag = "Player";
+
+	public override void activate(GameObject activator) {
+		if (activator.CompareTag(characterTag)) {
+			otherEnd.acceptEntityTeleport(activator);
+		}
+	}
+
+	public override void pickup(GameObject parentGameObject) {}
+
+	public override void drop(GameObject exParentGameObject) {}
+
+	public void acceptEntityTeleport(GameObject entity) {
+		entity.transform.position = teleportExitPosition;
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.DrawIcon(teleportExitPosition, "point.png");
+	}
+}
